8e89f7c28a5c3b7c80243676183222486486eb7e
Сергей <eonvse@ya.ru>
2024-12-17 19:34:10 +0800

    Добавлены компоненты представлений - button, input, marker, modal-wire

---
+++ resources/views/components/marker/role.blade.php
@@ -0,0 +1,15 @@
+@props(['name'])
+
+@php
+$type = empty($name) ? 'default' : explode(' ',$name)[0];
+$types = [
+    'default' => 'bg-blue-500 text-white',
+    'Roles' => 'bg-yellow-200 text-black',
+    'Tasks' => 'bg-sky-200 text-black',
+    'Users' => 'bg-green-200 text-black',
+    'Notes' => 'bg-orange-100 text-black',
+];
+$css = isset($types[$type]) ? $types[$type] : $types['default'];
+@endphp
+
+<span class="{{ $css }} rounded p-1">{{ $name }}</span>
