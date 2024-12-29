d370e1106469119dac6684f65bf45bccd4b33d9f
Сергей <eonvse@ya.ru>
2024-12-29 12:37:33 +0800

    Исправлена ошибка ссылки перехода на главный сайт с карточки логина.

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,9 +1,9 @@
-<a href="url('/')">
-<svg class="h-16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" {{ $attributes }}>  
-  <path stroke="none" d="M0 0h24v24H0z"/>  
-  <rect x="4" y="4" width="6" height="5" rx="2" />  
-  <rect x="4" y="13" width="6" height="7" rx="2" />  
-  <rect x="14" y="4" width="6" height="7" rx="2" />  
+<a href="{{ url('/') }}">
+<svg class="h-16" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round" {{ $attributes }}>
+  <path stroke="none" d="M0 0h24v24H0z"/>
+  <rect x="4" y="4" width="6" height="5" rx="2" />
+  <rect x="4" y="13" width="6" height="7" rx="2" />
+  <rect x="14" y="4" width="6" height="7" rx="2" />
   <rect x="14" y="15" width="6" height="5" rx="2" />
 </svg>
 </a>
