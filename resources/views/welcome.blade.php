5ba4d0f931a2dfccf03da8d51a3990dbe1930b7b
Сергей <eonvse@ya.ru>
2025-01-07 15:14:19 +0800

    Обновлена иконка приложения

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,8 +26,13 @@
                     <main class="mt-6">
                         <div class="grid gap-6 lg:gap-8">
                             <div class="flex flex-col items-start gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
-                                <div class="text-xl font-semibold">Система публикации материалов по группам. </div>
-                                <div class="">С управлением ролями и разбиением на команды (группы)</div>
+                                <div class="flex space-x-4">
+                                    <div><x-application-logo class="w-16 h-16" /></div>
+                                    <div>
+                                        <div class="text-xl font-semibold">Система публикации материалов по группам. </div>
+                                        <div class="">С управлением ролями и разбиением на команды (группы)</div>
+                                    </div>
+                                </div>
                             </div>
                             <div class="flex flex-col items-center gap-6 overflow-hidden rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] md:row-span-3 lg:p-10 lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]">
                                 @if (Route::has('login'))
